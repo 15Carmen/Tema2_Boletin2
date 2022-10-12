5d663675bbe37a20b34d4856f156c962edc78f04
15Carmen <carmen.martin@iesnervion.es>
2022-10-12 23:25:40 +0200

    commit3

---
+++ src/ejercicios/Ejercicio5.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+public class Ejercicio5 {
+    /*
+    Queremos saber, una vez introducido un carnet de conducir, a qué corresponde. Sabiendo:
+      a) E: remolques.
+      b) D: autobuses.
+      c) C1-C5: camiones.
+      d) A: motocicletas.
+      e) B1-B2: automóviles.
+      f) En caso de introducir uno distinto: “Categoría no contemplada”.
+
+     */
+    public static void main(String[] args) {
+
+    }
+}
